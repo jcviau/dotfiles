8626c8fe5f0c5c4498a6b87ad5c525548f0b6d02
Jean-Claude Viau <2657714+jcviau@users.noreply.github.com>
2020-04-07 11:08:48 -0400

    Improve portainer script

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -1,4 +1,14 @@
-docker volume create portainer_data
-docker run -d --name dashboard -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
-sleep 5s
+if [ ! "$(docker ps -a | grep dashboard)" ];
+then
+  echo 'Creating and starting the docker dashboard.'
+  docker volume create portainer_data
+  docker run -d --name dashboard -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+  sleep 5s
+elif [ "$(docker ps -aq -f status=exited -f name=dashboard)" ]
+then
+  echo 'Starting the docker dashboard.'
+  docker start dashboard
+  sleep 5s
+fi
+
 google-chrome http://localhost:9000
